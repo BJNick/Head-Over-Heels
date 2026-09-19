ecb121e18fc478436835c5fd803eb98cee5e489c
Mykyta Shvets <nikitashets28@gmail.com>
2026-09-19 13:26:30 -0600

    Sliding and heel

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,9 +6,18 @@ public class PlayerScript : MonoBehaviour {
     public float speed = 5f;
     public float jumpForce = 10f;
     
+    public float slideSpeed = 2f;
+    public float minSlideDuration = 0.5f;
+    public float maxSlideDuration = 3f;
+    public float slideTimeout = 1f;
+    private float slideStartTime = -1f;
+    private float slideStopTime = -1f;
+    private bool isSliding = false;
+    
     private Vector3 startPosition;
     
     private Animator animator;
+    
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,6 +30,12 @@ public class PlayerScript : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         float velX = ((Input.GetAxis("Horizontal")+1) * speed);
+        float slideTime = Time.time - slideStartTime;
+        
+        if (isSliding) {
+            velX *= slideSpeed;
+        }
+        
         rb.linearVelocityX = velX * speed;
         
         animator.speed = Input.GetAxis("Horizontal")+1;
@@ -32,11 +47,16 @@ public class PlayerScript : MonoBehaviour {
             transform.position = startPosition;
             rb.linearVelocity = Vector3.zero;
         }
-        if (Input.GetKeyDown(KeyCode.S)) {
+        if (Input.GetKey(KeyCode.S) && !isSliding && (Time.time - slideStopTime >= slideTimeout)) {
             animator.SetBool("Slide", true);
+            slideStartTime = Time.time;
+            isSliding = true;
         }
-        if (Input.GetKeyUp(KeyCode.S)) {
+        else if (isSliding && ((Input.GetKeyUp(KeyCode.S) && slideTime >= minSlideDuration) ||
+                          (slideTime >= maxSlideDuration))) {
             animator.SetBool("Slide", false);
+            isSliding = false;
+            slideStopTime = Time.time;
         }
     }
 }
