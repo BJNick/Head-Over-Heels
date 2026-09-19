9795fed5ec33e959708db2c9d6fa03de024bdbe6
Mykyta Shvets <nikitashets28@gmail.com>
2026-09-18 21:51:24 -0600

    Runnning fast

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,21 +1,30 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour
-{
-    Rigidbody2D rb;
+public class PlayerScript : MonoBehaviour {
+    private Rigidbody2D rb;
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    
+    private Vector3 startPosition;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        rb.linearVelocity = Vector2.right * Input.GetAxis("Horizontal");
+    void Update() {
+        float velX = (Input.GetAxis("Horizontal") * speed);
+        rb.linearVelocityX = velX * speed;
+        
         if (Input.GetKeyDown(KeyCode.Space)) {
-            rb.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.R)) {
+            transform.position = startPosition;
         }
     }
 }
