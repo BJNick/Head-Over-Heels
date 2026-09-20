9ea7f8fe8b57a14e01b55eb4407a29ded8c90284
Mykyta Shvets <nikitashets28@gmail.com>
2026-09-19 22:35:31 -0600

    Better shooting

--- Assets/Scripts/BackgroundPan.cs
+++ Assets/Scripts/BackgroundPan.cs
@@ -10,20 +10,22 @@ public class BackgroundPan : MonoBehaviour
     public float resetEvery = 10f;
     
     private Vector3 camStartPosition;
+    private Vector3 startPosition;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         camStartPosition = Camera.main.transform.position;
+        startPosition = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
         var diff = Camera.main.transform.position - camStartPosition;
-        var scrollX = (Camera.main.transform.position.x - Mathf.Repeat(diff.x * scrollRatio, resetEvery));
-        var scrollY = (Camera.main.transform.position.y - diff.y * scrollRatioY);
-        transform.position = new Vector3(scrollX, scrollY, transform.position.z);
+        var scrollX = startPosition.x - Mathf.Repeat(diff.x * scrollRatio, resetEvery);
+        var scrollY = startPosition.y - (diff.y * scrollRatioY);
+        transform.localPosition = new Vector3(scrollX, scrollY, transform.localPosition.z);
 
     }
 }
